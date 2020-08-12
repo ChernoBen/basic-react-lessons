fc770dc7b71f52bf463c1621966e7e0ba40455be
Benjamim <benjamim.francisco@hotmail.com>
2020-08-12 13:04:21 -0300

    integrando api e token

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ListComponent from './ListComponent';
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listName = {'Minha Lista'} />
-            <ListComponent listName = {'Minha Lista'} />
-        </div>
-    )
+//class que herda de react component
+export default class UserLists extends React.Component {
+    //integrando api de forma asincrona
+    state = {lists:null,loading:true}
+
+    async componentDidMount(){
+        //integrando token
+        const config = {
+            headers:{
+                'Content-type':'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token 85658bb0499387dcfa14a22775bb34bcc186e31c';
+        
+        //obtendo json do django server
+        var url = 'http://127.0.0.1:8000/list/';
+        //passando url e token como parametro 
+        const response = await fetch(url,config);
+        const data = await response.json();
+        console.log(data);
+        this.setState({lists:data,loading:false});
+        
+    }
+
+    render(){
+        return (
+            <div>
+                <ListComponent listName = {'Minha Lista'} />
+                <ListComponent listName = {'Minha Lista'} />
+            </div>
+        )
+    }
 }
